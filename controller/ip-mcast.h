a88bcd29c5ba8dda828c2626e00e1ecf5ed55c19
Mark Michelson <mmichels@redhat.com>
2024-01-19 15:57:19 -0500

    rbac: Restrict IGMP_Group updates to relevant chassis.
    RBAC did not restrict which chassis could update IGMP_Groups. With this
    change, we add a new "chassis_name" column to IGMP_Group.
    
    This may seem odd since there is already a "chassis" column in
    IGMP_Group. But RBAC specifically works by string matching based on the
    certificate common name. Therefore, we need to have a chassis_name
    string column instead of a chassis UUID column.

--- controller/ip-mcast.h
+++ controller/ip-mcast.h
@@ -39,11 +39,13 @@ struct sbrec_igmp_group *igmp_group_create(
     struct ovsdb_idl_txn *idl_txn,
     const struct in6_addr *address,
     const struct sbrec_datapath_binding *datapath,
-    const struct sbrec_chassis *chassis);
+    const struct sbrec_chassis *chassis,
+    bool igmp_group_has_chassis_name);
 struct sbrec_igmp_group *igmp_mrouter_create(
     struct ovsdb_idl_txn *idl_txn,
     const struct sbrec_datapath_binding *datapath,
-    const struct sbrec_chassis *chassis);
+    const struct sbrec_chassis *chassis,
+    bool igmp_group_has_chassis_name);
 
 void igmp_group_update_ports(const struct sbrec_igmp_group *g,
                              struct ovsdb_idl_index *datapaths,
