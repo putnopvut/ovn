0df46ab7e169cb331096599303567a664b19fe09
Mark Michelson <mmichels@redhat.com>
2025-05-29 11:04:51 -0400

    Datapath_Binding: Separate type and UUID external-ids.
    Prior to this commit, if you wanted to find the corresponding northbound
    UUID for a southbound Logical Datapath, you could find it in one of two
    places:
    
    For logical switches, it was in external-ids:logical-switch.
    For logical routers, it was in external-ids:logical-router.
    
    With this commit, we are separating the type and UUID. The type is a new
    column, and the datapath UUID is now propagated from the northbound logical
    datapath. There had been concerns about this UUID propagation in the
    past since it was possible for a logical switch and logical router to
    have the same UUID. While this was technically true in the northbound
    database, ovn-northd would flag this as an error and not allow the
    duplicated UUID. Therefore, we are not breaking any existing
    deployments with this change.
    
    Note that there is a seemingly-unrelated change in the check packet
    length test in tests/ovn.at. This test started failing because its use
    of `grep` was picking up the new "nb_uuid" column accidentally. The
    change to the test uses `fetch_column` since it is more precise.
    
    The ovn-northd duplicated datapaths test has been removed from the
    testsuite. The reason is that it is now impossible to create a
    duplicated datapath since the OVSDB server will not allow us to insert a
    datapath binding with a duplicated UUID.
    
    Signed-off-by: Mark Michelson <mmichels@redhat.com>

--- lib/ovn-util.h
+++ lib/ovn-util.h
@@ -546,4 +546,22 @@ void put_load(uint64_t value, enum mf_field_id dst, size_t ofs, size_t n_bits,
 #define _VFUNC(name, n) _VFUNC_(name, n)
 #define VFUNC(func, ...) _VFUNC(func, __NARG__(__VA_ARGS__)) (__VA_ARGS__)
 
+/* The two purposes for which ovn-northd uses OVN logical datapaths. */
+enum ovn_datapath_type {
+    DP_UNSPEC = 0,
+    DP_SWITCH = 1,                  /* OVN logical switch. */
+    DP_ROUTER = 2,                  /* OVN logical router. */
+    DP_MAX,
+};
+
+bool datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *sb,
+                                   struct uuid *nb_uuid,
+                                   enum ovn_datapath_type *type);
+
+bool datapath_get_nb_uuid(const struct sbrec_datapath_binding *sb,
+                          struct uuid *nb_uuid);
+
+enum ovn_datapath_type datapath_get_nb_type(
+    const struct sbrec_datapath_binding *sb);
+
 #endif /* OVN_UTIL_H */
