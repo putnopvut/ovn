putnopvut/ovn

commit ceea1d7cccad2f10840ceccff3d4553bfc1f222a

northd: Allow LB health checks from router IPs.
Load Balancer Health Checks require specifying "source IP". This IP
has to be from the subnet of the monitored LB backend and should not
be used by any other port in the LSP. If the "source IP" is also used
by some other port, the host with the LB backend won't be able to communicate
with this port due to the ARP responder rule installed by the controller.

This limitation forces CMSes to reserve an IP per LSP with LB backend, which
is not always practical or possible.

This change allows usage of LRP IPs as the source of LB health check
probes. It introduces following changes for such scenario:

  * service_monitor (SBDB) will use MAC address of the LRP
  * ARP responder rule is not necessary in this case
  * Destination IP, destination MAC, "inport" and source TCP/UDP port number
    will be used to determine that a packet is a response to a health check
    probe. These packets will be redirected to the controller.

Behavior is unchanged for the LB health checks that use reserved/unused
"source IPs".

Signed-off-by: Martin Kalcok <martin.kalcok@canonical.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/northd.c
@@ -3266,6 +3266,20 @@ ovn_lb_svc_create(struct ovsdb_idl_txn *ovnsb_txn,
                 continue;
             }
 
+            ovn_northd_lb_backend_set_mon_port(op, backend_nb);
+
+            /* If the service monitor is backed by a real port, use its MAC
+             * address instead of the default service check MAC. */
+            const struct eth_addr *source_mac_ea;
+            const char *source_mac;
+            if (backend_nb->svc_mon_lrp) {
+                source_mac_ea = &backend_nb->svc_mon_lrp->lrp_networks.ea;
+                source_mac = backend_nb->svc_mon_lrp->lrp_networks.ea_s;
+            } else {
+                source_mac_ea = svc_monitor_mac_ea;
+                source_mac = svc_monitor_mac;
+            }
+
             const char *protocol = lb->nlb->protocol;
             if (!protocol || !protocol[0]) {
                 protocol = "tcp";
@@ -3295,9 +3309,9 @@ ovn_lb_svc_create(struct ovsdb_idl_txn *ovnsb_txn,
             struct eth_addr ea;
             if (!mon_info->sbrec_mon->src_mac ||
                 !eth_addr_from_string(mon_info->sbrec_mon->src_mac, &ea) ||
-                !eth_addr_equals(ea, *svc_monitor_mac_ea)) {
+                !eth_addr_equals(ea, *source_mac_ea)) {
                 sbrec_service_monitor_set_src_mac(mon_info->sbrec_mon,
-                                                  svc_monitor_mac);
+                                                  source_mac);
             }
 
             if (!mon_info->sbrec_mon->src_ip ||
@@ -8516,6 +8530,102 @@ build_lb_rules_for_stateless_acl(struct lflow_table *lflows,
 }
 
 static void
+build_lb_health_check_response_lflows(
+    struct lflow_table *lflows,
+    const struct ovn_northd_lb *lb,
+    const struct ovn_lb_vip *lb_vip,
+    const struct ovn_northd_lb_vip *lb_vip_nb,
+    const struct ovn_lb_datapaths *lb_dps,
+    const struct ovn_datapaths *lr_datapaths,
+    const struct shash *meter_groups,
+    struct ds *match,
+    struct ds *action)
+{
+    /* For each LB backend that is monitored by a source_ip belonging
+     * to a real LRP, install rule that punts service check replies to the
+     * controller. */
+    const struct ovn_lb_backend *backend;
+    size_t j = 0;
+
+    VECTOR_FOR_EACH_PTR (&lb_vip->backends, backend) {
+        struct ovn_northd_lb_backend *backend_nb =
+            &lb_vip_nb->backends_nb[j++];
+
+        if (!backend_nb->health_check || !backend_nb->svc_mon_lrp) {
+            continue;
+        }
+
+        const char *protocol = lb->nlb->protocol;
+        if (!protocol || !protocol[0]) {
+            protocol = "tcp";
+        }
+
+        size_t index;
+        DYNAMIC_BITMAP_FOR_EACH_1 (index, &lb_dps->nb_lr_map) {
+            struct ovn_datapath *od = sparse_array_get(&lr_datapaths->dps,
+                                                       index);
+            if (!od) {
+                continue;
+            }
+
+            /* Check if the "service monitor source" LRP belongs to this
+             * router. */
+            if (backend_nb->svc_mon_lrp->od != od) {
+                continue;
+            }
+
+            /* Get the peer switch datapath where the backend is located. */
+            struct ovn_datapath *peer_switch_od = NULL;
+            if (backend_nb->svc_mon_lrp->peer) {
+                peer_switch_od = backend_nb->svc_mon_lrp->peer->od;
+            }
+
+            if (!peer_switch_od
+                || ovn_datapath_get_type(peer_switch_od) != DP_SWITCH) {
+                continue;
+            }
+
+            ds_clear(match);
+            ds_clear(action);
+
+            /* icmp6 type 1 and icmp4 type 3 are included in the match, because
+             * the controller is using them to detect unreachable ports. */
+            if (addr_is_ipv6(backend_nb->svc_mon_src_ip)) {
+                ds_put_format(match,
+                              "inport == \"%s\" && ip6.dst == %s && "
+                              "eth.dst == %s && "
+                              "(%s.src == %s || icmp6.type == 1)",
+                              backend_nb->logical_port,
+                              backend_nb->svc_mon_src_ip,
+                              backend_nb->svc_mon_lrp->lrp_networks.ea_s,
+                              protocol,
+                              backend->port_str);
+            } else {
+                ds_put_format(match,
+                              "inport == \"%s\" && ip4.dst == %s && "
+                              "eth.dst == %s && "
+                              "(%s.src == %s || icmp4.type == 3)",
+                              backend_nb->logical_port,
+                              backend_nb->svc_mon_src_ip,
+                              backend_nb->svc_mon_lrp->lrp_networks.ea_s,
+                              protocol,
+                              backend->port_str);
+            }
+
+            /* ovn-controller expects health check responses from the LS
+             * datapath in which the backend is located. That's why we
+             * install the response lflow into the peer's datapath. */
+            const char *meter = copp_meter_get(COPP_SVC_MONITOR,
+                                               peer_switch_od->nbs->copp,
+                                               meter_groups);
+            ovn_lflow_add(lflows, peer_switch_od, S_SWITCH_IN_L2_LKUP, 110,
+                          ds_cstr(match), "handle_svc_check(inport);",
+                          lb_dps->lflow_ref, WITH_CTRL_METER(meter));
+        }
+    }
+}
+
+static void
 build_lb_rules(struct lflow_table *lflows, struct ovn_lb_datapaths *lb_dps,
                const struct ovn_datapaths *ls_datapaths,
                struct ds *match, struct ds *action,
@@ -10362,6 +10472,12 @@ build_lswitch_arp_nd_local_svc_mon(const struct ovn_lb_datapaths *lb_dps,
             struct ovn_port *op = ovn_port_find(ls_ports,
                                                 backend_nb->logical_port);
             if (!op || !backend_nb->svc_mon_src_ip) {
+                continue;
+            }
+
+            /* ARP responder is only needed if the service check is NOT
+             * backed by a real LRP (i.e., using a reserved IP). */
+            if (backend_nb->svc_mon_lrp) {
                 continue;
             }
 
@@ -13159,6 +13275,10 @@ build_lrouter_flows_for_lb(struct ovn_lb_datapaths *lb_dps,
 
         build_lrouter_allow_vip_traffic_template(lflows, lb_dps, lb_vip, lb,
                                                  lr_datapaths);
+
+        build_lb_health_check_response_lflows(
+            lflows, lb, lb_vip, &lb->vips_nb[i], lb_dps, lr_datapaths,
+            meter_groups, match, action);
 
         if (!build_empty_lb_event_flow(lb_vip, lb, match, action)) {
             continue;

commit b698a97c2fb369e8091ee9b2759b0660e9cd608b

northd: Don't recognize UDP packets as health check responses.
UDP health checks rely on receiving either an ICMP error to indicate
that the service is down, or no response as a way to indicate that the
service is live.

If a particularly dumb service (for instance a UDP "echo" service) is
configured, it will send UDP responses to our health check probes. In
this case, we don't want to send the response to our health check
processing, because the health check does not expect to get a UDP
response. Instead, ignore the response and let it get dropped eventually
by the logical router.

Fixes: ceea1d7cccad ("northd: Allow LB health checks from router IPs.")
Reported-at: https://redhat.atlassian.net/browse/FDP-3435
Co-authored-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

northd/northd.c
@@ -8872,25 +8872,29 @@ build_lb_health_check_response_lflows(
             /* icmp6 type 1 and icmp4 type 3 are included in the match, because
              * the controller is using them to detect unreachable ports. */
             if (addr_is_ipv6(backend_nb->svc_mon_src_ip)) {
-                ds_put_format(match,
-                              "inport == \"%s\" && ip6.dst == %s && "
-                              "eth.dst == %s && "
-                              "(%s.src == %s || icmp6.type == 1)",
+                ds_put_format(match, "inport == \"%s\" && ip6.dst == %s && "
+                              "ip6.src == %s && eth.dst == %s && ",
                               backend_nb->logical_port,
                               backend_nb->svc_mon_src_ip,
-                              backend_nb->svc_mon_lrp->lrp_networks.ea_s,
-                              protocol,
-                              backend->port_str);
+                              backend->ip_str,
+                              backend_nb->svc_mon_lrp->lrp_networks.ea_s);
+                if (!strcmp(protocol, "tcp")) {
+                    ds_put_format(match, "tcp.src == %s", backend->port_str);
+                } else {
+                    ds_put_cstr(match, "icmp6.type == 1");
+                }
             } else {
-                ds_put_format(match,
-                              "inport == \"%s\" && ip4.dst == %s && "
-                              "eth.dst == %s && "
-                              "(%s.src == %s || icmp4.type == 3)",
+                ds_put_format(match, "inport == \"%s\" && ip4.dst == %s && "
+                              "ip4.src == %s && eth.dst == %s && ",
                               backend_nb->logical_port,
                               backend_nb->svc_mon_src_ip,
-                              backend_nb->svc_mon_lrp->lrp_networks.ea_s,
-                              protocol,
-                              backend->port_str);
+                              backend->ip_str,
+                              backend_nb->svc_mon_lrp->lrp_networks.ea_s);
+                if (!strcmp(protocol, "tcp")) {
+                    ds_put_format(match, "tcp.src == %s", backend->port_str);
+                } else {
+                    ds_put_cstr(match, "icmp4.type == 3");
+                }
             }
 
             /* ovn-controller expects health check responses from the LS

tests/ovn.at
@@ -27208,20 +27208,11 @@ AT_CHECK([grep "priority=110" sbflows2 | grep "handle_svc_check" | \
           grep 'inport == "sw1-p1"' | grep "ip4.dst == 20.0.0.1" | \
           grep -q "tcp.src == 80" ])
 
-# Verify flow also matches ICMP unreachable (type 3 for IPv4).
-AT_CHECK([grep "priority=110" sbflows1 | grep "handle_svc_check" | \
-          grep 'inport == "sw0-p1"' | grep "ip4.dst == 10.0.0.1" | \
-          grep -q "icmp4.type == 3"])
-
-AT_CHECK([grep "priority=110" sbflows2 | grep "handle_svc_check" | \
-          grep 'inport == "sw1-p1"' | grep "ip4.dst == 20.0.0.1" | \
-          grep -q "icmp4.type == 3"])
-
 ## UDP Load balancer health check flows.
-# UDP backend - verify flow uses UDP instead of TCP.
+# UDP backend - verify flow uses ICMP type 3 instead of TCP.
 AT_CHECK([grep "priority=110" sbflows1 | grep "handle_svc_check" | \
           grep 'inport == "sw0-p2"' | grep "ip4.dst == 10.0.0.1" | \
-          grep -q "udp.src == 53"])
+          grep -q "icmp4.type == 3"])
 
 # Verify NO TCP match in the UDP backend flow.
 AT_CHECK([grep "priority=110" sbflows1 | grep "handle_svc_check" | \
@@ -27370,20 +27361,11 @@ AT_CHECK([grep "priority=110" sbflows2 | grep "handle_svc_check" | \
           grep 'inport == "sw1-p1"' | grep "ip6.dst == 2002::1" | \
           grep -q "tcp.src == 80" ])
 
-# Verify flow also matches ICMP unreachable (type 1 for IPv6).
-AT_CHECK([grep "priority=110" sbflows1 | grep "handle_svc_check" | \
-          grep 'inport == "sw0-p1"' | grep "ip6.dst == 2001::1" | \
-          grep -q "icmp6.type == 1"])
-
-AT_CHECK([grep "priority=110" sbflows2 | grep "handle_svc_check" | \
-          grep 'inport == "sw1-p1"' | grep "ip6.dst == 2002::1" | \
-          grep -q "icmp6.type == 1"])
-
 ## UDP Load balancer health check flows.
-# UDP backend - verify flow uses UDP instead of TCP.
+# UDP backend - verify flow uses ICMP instead of TCP.
 AT_CHECK([grep "priority=110" sbflows1 | grep "handle_svc_check" | \
           grep 'inport == "sw0-p2"' | grep "ip6.dst == 2001::1" | \
-          grep -q "udp.src == 53"])
+          grep -q "icmp6.type == 1"])
 
 # Verify NO TCP match in the UDP backend flow.
 AT_CHECK([grep "priority=110" sbflows1 | grep "handle_svc_check" | \

tests/system-ovn.at
@@ -21496,3 +21496,90 @@ OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
 /connection dropped.*/d"])
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([Unsupported protocol message])
+AT_SKIP_IF([test $HAVE_NC = no])
+
+ovn_start
+OVS_TRAFFIC_VSWITCHD_START()
+ADD_BR([br-int])
+
+# Set external-ids in br-int needed for ovn-controller.
+check ovs-vsctl \
+        -- set Open_vSwitch . external-ids:system-id=hv1 \
+        -- set Open_vSwitch . external-ids:ovn-remote=unix:$ovs_base/ovn-sb/ovn-sb.sock \
+        -- set Open_vSwitch . external-ids:ovn-encap-type=geneve \
+        -- set Open_vSwitch . external-ids:ovn-encap-ip=169.0.0.1 \
+        -- set bridge br-int fail-mode=secure other-config:disable-in-band=true
+
+# Start ovn-controller.
+start_daemon ovn-controller
+
+check ovn-nbctl ls-add ls1
+check ovn-nbctl lsp-add ls1 ls1p1
+check ovn-nbctl lsp-set-addresses ls1p1 "00:00:00:01:01:01 192.168.1.1"
+check ovn-nbctl lsp-add ls1 ls1p2
+check ovn-nbctl lsp-set-addresses ls1p2 "00:00:00:01:01:02 192.168.1.2"
+
+check ovn-nbctl lr-add lr1
+check ovn-nbctl lrp-add lr1 lr1-ls1 00:00:00:00:00:01 192.168.1.254/24
+check ovn-nbctl lsp-add ls1 ls1-lr1
+check ovn-nbctl lsp-set-addresses ls1-lr1 "00:00:00:00:00:01 192.168.1.254"
+check ovn-nbctl lsp-set-type ls1-lr1 router
+check ovn-nbctl lsp-set-options ls1-lr1 router-port=lr1-ls1
+
+check ovn-nbctl lrp-add lr1 lr1-ls2 00:00:00:00:00:02 192.168.2.254/24
+
+check ovn-nbctl ls-add ls2
+check ovn-nbctl lsp-add ls2 ls2-lr1
+check ovn-nbctl lsp-set-addresses ls2-lr1 "00:00:00:00:00:02 192.168.2.254"
+check ovn-nbctl lsp-set-type ls2-lr1 router
+check ovn-nbctl lsp-set-options ls2-lr1 router-port=lr1-ls2
+
+check ovn-nbctl lsp-add ls2 ls2p1
+check ovn-nbctl lsp-set-addresses ls2p1 "00:00:00:01:02:01 192.168.2.1"
+
+ADD_NAMESPACES(ls1p1)
+ADD_VETH(ls1p1, ls1p1, br-int, "192.168.1.1/24", "00:00:00:01:01:01",
+         "192.168.1.254")
+
+ADD_NAMESPACES(ls2p1)
+ADD_VETH(ls2p1, ls2p1, br-int, "192.168.2.1/24", "00:00:00:01:02:01",
+         "192.168.2.254")
+
+ADD_NAMESPACES(ls1p2)
+ADD_VETH(ls1p2, ls1p2, br-int, "192.168.1.2/24", "00:00:00:01:01:02",
+         "192.168.1.254")
+
+check ovn-nbctl lb-add lb0 192.168.5.1:12345 192.168.1.1:12345,192.168.1.2:12345
+check ovn-nbctl ls-lb-add ls1 lb0
+check ovn-nbctl lr-lb-add lr1 lb0
+lb_uuid=$(fetch_column nb:Load_Balancer _uuid name=lb0)
+check ovn-nbctl set Load_Balancer $lb_uuid protocol=udp
+check ovn-nbctl --wait=hv set Logical_Router lr1 options:chassis="hv1"
+
+wait_for_ports_up ls1p1 ls1p2 ls2p1
+
+NETNS_DAEMONIZE([ls1p1], [nc -l 12345 --udp -k --sh-exec ls], [nc1.pid])
+NETNS_DAEMONIZE([ls1p2], [nc -l 12345 --udp -k --sh-exec ls], [nc2.pid])
+
+hc_uuid=$(ovn-nbctl --id=@hc create Load_Balancer_Health_Check vip="192.168.5.1\:12345" -- \
+          add Load_Balancer $lb_uuid health_check @hc)
+check ovn-nbctl set Load_Balancer_Health_Check $hc_uuid options:timeout=20 options:success_count=3 options:failure_count=3
+check ovn-nbctl --wait=sb set load_balancer $lb_uuid ip_port_mappings:192.168.1.1=ls1p1:192.168.1.254
+
+NS_EXEC([ls2p1], [nc --udp 192.168.5.1 12345 <<< h])
+
+# It may not seem like we're actually testing anything in this test.
+# If there is a warning or error in the ovn-controller log about
+# an unsupported health check protocol, it will cause a test failure
+# when we stop ovn-controller.
+OVN_CLEANUP_CONTROLLER([hv1])
+OVN_CLEANUP_NORTHD
+
+as
+OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
+/connection dropped.*/d"])
+AT_CLEANUP
+])